063de698167a7c0c6dda384e9dac702bb6ae46c7
melodeenae5 <danaekirby5@gmail.com>
2020-12-17 14:09:19 -0600

    changed apiUrl

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const apiUrl =
 	window.location.hostname === 'localhost'
 		? 'http://localhost:8000'
-		: 'https://danaekirby5-goals-backend.herokuapp.com/';
+		: 'https://danaekirby5-goals-backend.herokuapp.com';
