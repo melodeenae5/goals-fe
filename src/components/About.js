9b4b8e9d66276801bfa4abdfcdfa6adab787821e
melodeenae5 <danaekirby5@gmail.com>
2020-12-17 13:01:41 -0600

    finished about component

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,11 @@ const About = () => {
 	return (
 		<div>
 			<h4>About</h4>
+			<p>
+				Hello! Welcome to 'Achieve Your Goal'! This is a site where you can log
+				your goals, make a checklist of tasks to be completed, and draft notes
+				about your progress. Please make an account and login to get started!{' '}
+			</p>
 		</div>
 	);
 };
